6a872442755f77c1fbb74ad36ed5f979ddd9eb7b
minematsushun <50306222+minematsushun@users.noreply.github.com>
2019-12-12 19:15:01 +0900

    メール問い合わせ設定

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -2,7 +2,7 @@ class InquiryMailer < ApplicationMailer
   def send_mail(inquiry)
     @inquiry = inquiry
     mail(
-      from: 'system@example.com',
+      from: 'o62.minematsu.shunya@gmail.com',
       to: 'o62.minematsu.shunya@gmail.com',
       subject: 'お問合せ通知'
     )
