ec054194538218973553cecbd662a01d3ec958a6
minematsushun <50306222+minematsushun@users.noreply.github.com>
2019-12-13 19:26:40 +0900

    環境変数

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -2,8 +2,7 @@ class InquiryMailer < ApplicationMailer
   def send_mail(inquiry) #自分用
     @inquiry = inquiry
     mail(
-      # to: ENV['MAIL'],
-      to: 'o62.minematsu.shunya@gmail.com',
+      to: Rails.application.credentials.gmail[:user_name],
       subject: 'お問合せ通知'
     )
   end
