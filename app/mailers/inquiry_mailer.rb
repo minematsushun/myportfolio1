a167bffd0740b9bac95a7b400ac155efba03f347
minematsushun <50306222+minematsushun@users.noreply.github.com>
2019-12-13 19:33:49 +0900

    envに戻す

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -2,7 +2,7 @@ class InquiryMailer < ApplicationMailer
   def send_mail(inquiry) #自分用
     @inquiry = inquiry
     mail(
-      to: Rails.application.credentials.gmail[:user_name],
+      to: ENV['MAIL'],
       subject: 'お問合せ通知'
     )
   end
