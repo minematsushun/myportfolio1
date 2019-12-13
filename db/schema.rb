0addbe479445370dbdb502542307c63115ac03d3
minematsushun <50306222+minematsushun@users.noreply.github.com>
2019-12-13 16:03:15 +0900

    自動

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2019_12_12_082839) do
   create_table "inquiries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "message"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
