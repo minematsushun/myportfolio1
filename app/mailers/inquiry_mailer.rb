29260c5f6c189e66610c77d0fcebaec39f1990e1
minematsushun <50306222+minematsushun@users.noreply.github.com>
2019-12-13 13:52:10 +0900

    環境変数に変更

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -2,8 +2,7 @@ class InquiryMailer < ApplicationMailer
   def send_mail(inquiry)
     @inquiry = inquiry
     mail(
-      from: 'o62.minematsu.shunya@gmail.com',
-      to: 'o62.minematsu.shunya@gmail.com',
+      to: ENV['MAIL'],
       subject: 'お問合せ通知'
     )
   end
