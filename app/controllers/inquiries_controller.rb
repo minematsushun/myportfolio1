bde0c1dff97a0b7e2efddd9115f12b078226d68f
minematsushun <50306222+minematsushun@users.noreply.github.com>
2019-12-28 12:57:59 +0900

    alert 変更

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -9,8 +9,7 @@ class InquiriesController < ApplicationController
       InquiryMailer.send_mail(@inquiry).deliver_now
       redirect_to root_path, notice: 'お問合せを受け付けました'
     else
-      render :index
-      flash[:alert] = "必須項目を入力してください。"
+      redirect_to root_path, alert: '必須項目を入力してください。'
     end
   end
 
